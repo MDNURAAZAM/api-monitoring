5d10841ac881527598891a32f86f25efc6f0726b
MDNURAAZAM <md.nura.azam@g.bracu.ac.bd>
2025-03-16 14:19:26 +0600

    add environment configurations and update server to use dynamic port

---
+++ helpers/environments.js
@@ -0,0 +1,37 @@
+/*
+ * Title: Environments
+ * Description: enviorment configurations
+ * Author: MD Nura Azam
+ * Date: 16/03/2025
+ *
+ */
+
+// module scaffolding
+const environments = {};
+
+//staging environment
+environments.staging = {
+  port: 3000,
+  envName: "staging",
+};
+
+//production environment
+environments.production = {
+  port: 5000,
+  envName: "production",
+};
+
+//determine which environment was passed
+const currentEnvironment =
+  typeof process.env.NODE_ENV === "string"
+    ? process.env.NODE_ENV.trim()
+    : "staging";
+
+//export corresponding environment object
+const environmentToExport =
+  typeof environments[currentEnvironment] === "object"
+    ? environments[currentEnvironment]
+    : environments.staging;
+
+//export module
+module.exports = environmentToExport;
--- index.js
+++ index.js
@@ -9,20 +9,19 @@
 // Dependencies
 const http = require("http");
 const { handleReqRes } = require("./helpers/handleReqRes");
+const environment = require("./helpers/environments");
 
 //app object - module scaffolding
 const app = {};
 
 // Configuration
-app.config = {
-  port: 3000,
-};
+app.config = {};
 
 // Create server
 app.createServer = () => {
   const server = http.createServer(app.handleRequest);
-  server.listen(app.config.port, () => {
-    console.log(`Server is running on port ${app.config.port}`);
+  server.listen(environment.port, () => {
+    console.log(`Server is running on port ${environment.port}`);
   });
 };
 
