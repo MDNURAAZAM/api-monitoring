4666482cd840a4c67f3e420acd9a8350b7a098e7
MDNURAAZAM <md.nura.azam@g.bracu.ac.bd>
2025-03-20 19:14:45 +0600

    add put endpoint for check route

--- handlers/routeHandlers/checkHandler.js
+++ handlers/routeHandlers/checkHandler.js
@@ -201,7 +201,114 @@ handler._check.get = (requestProperties, callback) => {
 };
 
 //put request handler
-handler._check.put = (requestProperties, callback) => {};
+handler._check.put = (requestProperties, callback) => {
+  const { body } = requestProperties || {};
+  //validate inputs
+  const id =
+    typeof body.id === "string" && body.id.trim().length == 20
+      ? body.id
+      : false;
+
+  const protocol =
+    typeof body.protocol === "string" &&
+    ["http", "https"].indexOf(body?.protocol?.trim()) >= 0
+      ? body.protocol
+      : false;
+
+  const url =
+    typeof body?.url === "string" && body?.url?.trim().length > 0
+      ? body?.url
+      : false;
+
+  const method =
+    typeof body?.method === "string" &&
+    ["GET", "POST", "PUT", "DELETE"].indexOf(
+      body?.method?.toUpperCase().trim()
+    ) >= 0
+      ? body.method.toUpperCase()
+      : false;
+
+  const successCodes =
+    typeof body?.successCodes === "object" &&
+    body?.successCodes instanceof Array
+      ? body.successCodes
+      : false;
+
+  const timeoutSeconds =
+    typeof body.timeoutSeconds === "number" &&
+    body.timeoutSeconds % 1 == 0 &&
+    body.timeoutSeconds >= 1 &&
+    body.timeoutSeconds <= 5
+      ? body.timeoutSeconds
+      : false;
+
+  if (id) {
+    data.read("checks", id, (err1, checkData) => {
+      if (!err1 && checkData) {
+        const checkObject = parseJSON(checkData);
+        //validate the token
+        const token =
+          typeof requestProperties.headersObject.token === "string" &&
+          requestProperties.headersObject.token.trim().length === 20
+            ? requestProperties.headersObject.token.trim()
+            : false;
+        tokenHandler._token.verify(
+          token,
+          checkObject.userPhone,
+          (isTokenValid) => {
+            if (isTokenValid) {
+              if (protocol || url || method || successCodes || timeoutSeconds) {
+                if (protocol) {
+                  checkObject.protocol = protocol;
+                }
+
+                if (url) {
+                  checkObject.url = url;
+                }
+
+                if (method) {
+                  checkObject.method = method;
+                }
+
+                if (successCodes) {
+                  checkObject.successCodes = successCodes;
+                }
+
+                if (timeoutSeconds) {
+                  checkObject.timeoutSeconds = timeoutSeconds;
+                }
+                //store the check object
+                data.update("checks", id, checkObject, (err2) => {
+                  if (!err2) {
+                    callback(200);
+                  } else {
+                    callback(500, {
+                      error: "There was a problem in the server side",
+                    });
+                  }
+                });
+              } else {
+                callback(400, {
+                  error: "You must provide at least one field to update",
+                });
+              }
+            } else {
+              callback(403, { error: "Authentication failure!" });
+            }
+          }
+        );
+      } else {
+        callback(400, {
+          error: "You have a problem in your request",
+        });
+      }
+    });
+  } else {
+    callback(400, {
+      error: "You have a problem in your request",
+    });
+  }
+};
 
 //delete request handler
 handler._check.delete = (requestProperties, callback) => {};
